16ced82a47f105443bbc09b5ace5f450a86e8ac9
mahsa <mkmi7171@gmail.com>
2025-07-25 18:11:56 -0400

    changed sanity.tsx

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const sanity = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.SANITY_API_VERSION, 
   useCdn: false, 
   token: process.env.SANITY_API_TOKEN, 
